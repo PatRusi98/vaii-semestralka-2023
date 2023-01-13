4c7f008a166a3831dda838d5b9f5518b9e675c5a
Patrik <patrik.rusinak@gmail.com>
2023-01-13 14:57:01 +0100

    simulator table created

--- resources/views/admin/cars/create.blade.php
+++ resources/views/admin/cars/create.blade.php
@@ -9,43 +9,31 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
             <form>
-                <div class="grid gap-6 mb-6 md:grid-cols-2">
+                <div class="grid gap-6 mb-6 md:grid-cols-1">
                     <div>
-                        <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">First name</label>
-                        <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John" required>
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Car Name</label>
+                        <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Callaway Corvette C7.R" required>
                     </div>
                     <div>
-                        <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900">Last name</label>
-                        <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Doe" required>
+                        <label for="simulator" class="block mb-2 text-sm font-medium text-gray-900">Simulator</label>
+                        <select id="simulator" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                            <option selected disabled>Choose simulator</option>
+                            <option value="rFactor 2">rFactor 2</option>
+                            <option value="Assetto Corsa">Assetto Corsa</option>
+                            <option value="Assetto Corsa Competizione">Assetto Corsa Competizione</option>
+                            <option value="iRacing">iRacing</option>
+                        </select>
                     </div>
                     <div>
-                        <label for="company" class="block mb-2 text-sm font-medium text-gray-900">Company</label>
-                        <input type="text" id="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Flowbite" required>
+                        <label for="carclass" class="block mb-2 text-sm font-medium text-gray-900">Class</label>
+                        <select id="carclass" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                            <option selected disabled>Choose class</option>
+                            <option value="GTE">GTE</option>
+                            <option value="GT3">GT3</option>
+                            <option value="HyperCar">HyperCar</option>
+                            <option value="LMP2">LMP2</option>
+                        </select>
                     </div>
-                    <div>
-                        <label for="phone" class="block mb-2 text-sm font-medium text-gray-900">Phone number</label>
-                        <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required>
-                    </div>
-                    <div>
-                        <label for="website" class="block mb-2 text-sm font-medium text-gray-900">Website URL</label>
-                        <input type="url" id="website" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="flowbite.com" required>
-                    </div>
-                    <div>
-                        <label for="visitors" class="block mb-2 text-sm font-medium text-gray-900">Unique visitors (per month)</label>
-                        <input type="number" id="visitors" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="" required>
-                    </div>
-                </div>
-                <div class="mb-6">
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email address</label>
-                    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="john.doe@company.com" required>
-                </div>
-                <div class="mb-6">
-                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                    <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required>
-                </div>
-                <div class="mb-6">
-                    <label for="confirm_password" class="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
-                    <input type="password" id="confirm_password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="•••••••••" required>
                 </div>
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
